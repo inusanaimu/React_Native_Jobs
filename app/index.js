c70c02521421726fd12f727090b245d3b093b861
Inusa Naimu <62981083+inusanaimu@users.noreply.github.com>
2023-05-31 16:32:03 +0000

    close

--- app/index.js
+++ app/index.js
@@ -1,10 +1,29 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components'
+
+
+import {COLORS, icons, images, SIZES } from '../constants'
 
 const Home = () => {
+    const router = useRouter();
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView style={{flex:1, backgroundColor: COLORS.lightWhite}}>
+            <Stack.Screen
+            options={{
+                headerStyle: {backgroundColor: COLORS.lightWhite},
+                headerShadowVisible:false,
+                headerLeft: () => (
+                    <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%"/>
+                ),
+                headerRight: () => (
+                    <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%"/>
+                ),
+
+            }}
+            />
+        </SafeAreaView>
     )
 }
 
