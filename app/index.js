f083efe1511fe99c02188f6d7b3e7bec0b33637e
Inusa Naimu <62981083+inusanaimu@users.noreply.github.com>
2023-06-01 11:33:36 +0000

    Done ScreenHeaderBtn

--- app/index.js
+++ app/index.js
@@ -1,24 +1,26 @@
 import { useState } from "react";
-import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { View, ScrollView, SafeAreaView } from "react-native";
 import { Stack, useRouter } from "expo-router";
-import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components'
 
-
-import {COLORS, icons, images, SIZES } from '../constants'
+import { COLORS, icons, images, SIZES } from '../constants'
+import { 
+    Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome 
+    } from '../components'
 
 const Home = () => {
     const router = useRouter();
+
     return (
-        <SafeAreaView style={{flex:1, backgroundColor: COLORS.lightWhite}}>
+        <SafeAreaView style={{flex:1, backgroundColor: COLORS.lightWhite }}>
             <Stack.Screen
             options={{
                 headerStyle: {backgroundColor: COLORS.lightWhite},
                 headerShadowVisible:false,
                 headerLeft: () => (
-                    <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%"/>
+                    <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
                 ),
                 headerRight: () => (
-                    <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%"/>
+                    <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
                     ),
                 headerTitle : "",
 
@@ -26,6 +28,15 @@ const Home = () => {
             />
 
             <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{
+                    flex:1,
+                    padding: SIZES.medium
+                }}>
+                    <Welcome
+                    />
+                    <Popularjobs/>
+                    <Nearbyjobs/>
+                </View>
                 
             </ScrollView>
 
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
+import {useState} from 'react'
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
+import { useRouter } from 'expo-router'
+import { icons, SIZES } from '../../../constants'
 import styles from './welcome.style'
 
-const Welcome = () => {
+const Welcome = (user = "John Doe") => {
+  const router = useRouter();
   return (
     <View>
-      <Text>Welcome</Text>
+     <View style={styles.container}>
+      <Text style={styles.userName}>Hello John Doe</Text>
+      <Text style={styles.welcomeMessage}>Find your Perfect jobs</Text>
+     </View>
+
+    <View style={styles.searchContainer}>
+      <View style={styles.searchWrapper}>
+        <TextInput style={styles.searchInput}/>
+      </View>
+
+    </View>
+
     </View>
   )
 }
