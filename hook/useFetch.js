cd4ac870e7abfd29b31cc680ac05de143bf98b68
Inusa Naimu <62981083+inusanaimu@users.noreply.github.com>
2023-06-01 16:25:34 +0000

    useFetch hook

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -4,5 +4,28 @@ import axios from "axios";
 const useFetch = () =>{
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null)
+    const [error, setError] = useState(null);
+
+    const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://jsearch.p.rapidapi.com/search',
+  params: {
+    query: 'Python developer in Texas, USA',
+    page: '1',
+    num_pages: '1'
+  },
+  headers: {
+    'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+    'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+  }
+};
+
+// try {
+// 	const response = await axios.request(options);
+// 	console.log(response.data);
+// } catch (error) {
+// 	console.error(error);
+// }
 }
