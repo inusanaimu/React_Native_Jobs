f873279fc7bcdf5ae0264399b32f57d19b89200a
Inusa Naimu <62981083+inusanaimu@users.noreply.github.com>
2023-06-03 09:37:25 +0000

    Job Details Page.

---
+++ app/job-details/[id].js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Text, View, SafeAreaView, ScrollView, ActivityIndicator, RefreshControl} from 'react-native'
+const JobDetails = () => {
+  return (
+    <View>
+
+    <Text>
+      JobDetails
+    </Text>
+    </View>
+  )
+}
+
+export default JobDetails
