83d94105ed7d708064b700a500d76316e958fece
Inusa Naimu <62981083+inusanaimu@users.noreply.github.com>
2023-06-03 11:06:20 +0000

    Welcome Page Job Search Function

--- app/index.js
+++ app/index.js
@@ -1,47 +1,58 @@
 import { useState } from "react";
-import { View, ScrollView, SafeAreaView } from "react-native";
+import { SafeAreaView, ScrollView, View } from "react-native";
 import { Stack, useRouter } from "expo-router";
 
-import { COLORS, icons, images, SIZES } from '../constants'
-import { 
-    Nearbyjobs, Popularjobs as Popularizes, ScreenHeaderBtn, Welcome 
-    } from '../components'
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularizes as Popularizes,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
-    const router = useRouter();
+  const router = useRouter()
+  const [searchTerm, setSearchTerm] = useState("");
 
-    return (
-        <SafeAreaView style={{flex:1, backgroundColor: COLORS.lightWhite }}>
-            <Stack.Screen
-            options={{
-                headerStyle: {backgroundColor: COLORS.lightWhite},
-                headerShadowVisible:false,
-                headerLeft: () => (
-                    <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
-                ),
-                headerRight: () => (
-                    <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
-                    ),
-                headerTitle : "",
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension='100%' />
+          ),
+          headerTitle: "",
+        }}
+      />
 
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+          <Welcome
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            handleClick={() => {
+              if (searchTerm) {
+                router.push(`/search/${searchTerm}`)
+              }
             }}
-            />
+          />
 
-            <ScrollView showsVerticalScrollIndicator={false}>
-                <View style={{
-                    flex:1,
-                    padding: SIZES.medium
-                }}>
-                    <Welcome
-                    />
-                    <Popularizes/>
-                    <Nearbyjobs/>
-                </View>
-                
-            </ScrollView>
+          <Popularizes />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-        </SafeAreaView>
-    )
-}
-
-export default Home
+export default Home;
