b4adc93a99092f765b3d9ffd00e89c891a2e3a11
Inusa Naimu <inusanaimu@gmail.com>
2023-06-04 14:57:29 +0100

    Personalise

--- app/index.js
+++ app/index.js
@@ -1,42 +1,38 @@
-import { useState } from "react";
+ import { useState } from "react";
 import { View, ScrollView, SafeAreaView } from "react-native";
 import { Stack, useRouter } from "expo-router";
 
-import { COLORS, icons, images, SIZES } from "../constants";
-import {
-  Nearbyjobs,
-  Popularizes as Popularizes,
-  ScreenHeaderBtn,
-  Welcome,
-} from "../components";
+import { COLORS, icons, images, SIZES } from '../constants'
+import { 
+    Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome 
+    } from '../components'
 
 const Home = () => {
-  const router = useRouter()
-  const [searchTerm, setSearchTerm] = useState("");
+    const router = useRouter();
 
-  return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-      <Stack.Screen
-        options={{
-          headerStyle: { backgroundColor: COLORS.lightWhite },
-          headerShadowVisible: false,
-          headerLeft: () => (
-            <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />
-          ),
-          headerRight: () => (
-            <ScreenHeaderBtn iconUrl={images.profile} dimension='100%' />
-          ),
-          headerTitle: "",
-        }}
-      />
+    return (
+        <SafeAreaView style={{flex:1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen
+            options={{
+                headerStyle: {backgroundColor: COLORS.lightWhite},
+                headerShadowVisible:false,
+                headerLeft: () => (
+                    <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                ),
+                headerRight: () => (
+                    <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+                    ),
+                headerTitle : "",
+
+            }}
+            />
 
             <ScrollView showsVerticalScrollIndicator={false}>
                 <View style={{
                     flex:1,
                     padding: SIZES.medium
                 }}>
-                    <Welcome
-                    />
+                    <Welcome/>
                     <Popularjobs/>
                     <Nearbyjobs/>
                 </View>
@@ -47,4 +43,4 @@ const Home = () => {
     )
 }
 
-export default Home;
+export default Home
